800a24e640c00c71a91401832f0930ee4b8e9807
Matheus Pontes Agostinho <matheuspontes96@hotmail.com>
2015-12-22 01:43:27 -0300

    funções dadas

--- ex29.c
+++ ex29.c
@@ -1,8 +1,9 @@
 /* Exercício(ex29.c) feito em linguagem C, baseando-se no jogo da velha.
  * Autores: Matheus Pontes
  * Professor: Ruben Carlo Benante
+ *
+ *OBS: Esse programa é protegido pela GNU LICENSE V2.0, para maiores informações consulte o arquivo LICENSE.
  */
-
 
 // Possíveis bibliotecas.
 #include <stdio.h>
@@ -10,3 +11,16 @@
 #include <time.h>
 #include <locale.h>
 #include <wchar.h>
+
+#define DIMTAB 3 // Dimensão da tabela do jogo da velha (3x3).
+
+typedef struct
+{
+    int dim[3][3];
+    int nivel; 
+}Dados;
+
+// Funções estabelecidas.
+int nivelfacil(int tab[3][3],int vez); // Nível fácil 
+int nivelmedio(int tab[3][3],int vez); // Nível médio 
+int niveldificil(int tab[3][3],int vez); // Nível difícil 
