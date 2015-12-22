e75cf1256b69f1b61f8295ab0a849bb293e62f2b
Matheus Pontes Agostinho <matheuspontes96@hotmail.com>
2015-12-22 01:05:53 -0300

    criação do ex29.c e início do desenvolvimento

---
+++ ex29.c
@@ -0,0 +1,12 @@
+/* Exercício(ex29.c) feito em linguagem C, baseando-se no jogo da velha.
+ * Autores: Matheus Pontes
+ * Professor: Ruben Carlo Benante
+ */
+
+
+// Possíveis bibliotecas.
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+#include <wchar.h>
